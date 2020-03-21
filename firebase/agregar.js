64ae1461ba4ba7a32678dfe9cae9d6136a63c141
Juan Reyes <juanp.reyesc@yahoo.com>
2020-03-20 23:49:07 -0600

    firebase practica 2

---
+++ firebase/agregar.js
@@ -0,0 +1,9 @@
+  
+    formulario.addEventListener('submit',(e)=> {
+        e.preventDefault();
+        var registro = new Registro(null,formulario.nombre.value,formulario.codigo.value);
+        registro.agregar();
+        formulario.nombre.value ='';
+        formulario.codigo.value ='';
+        $('#ventananuevo').modal('hide');
+    });
